0bd3d8186a830e039dec216ef3502eb6e6203d5f
kastastin <kostya242421@gmai.com>
2020-12-20 21:29:10 +0200

    code refactoring

--- script.js
+++ script.js
@@ -3,30 +3,27 @@ const loader = document.getElementById('loader');
 
 let ready = false;
 let imagesLoaded = 0;
-let totalImages = 0; 
+let totalImages = 0;
 let photosArray = [];
-let initialLoad = true;
 
 // Unsplash API
-let count = 5;
-const apiKey = 'pcPMSXjNR7ZqlMYq-Dmp2URNUl7gS9cUIcAR3wyIPqM'; 
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+const count = 30;
+const apiKey = 'pcPMSXjNR7ZqlMYq-Dmp2URNUl7gS9cUIcAR3wyIPqM';
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
-// Check if all images were loaded 
+// Check if all images were loaded
 function imageLoaded() {
     imagesLoaded++;
     if (imagesLoaded === totalImages) {
         ready = true;
         loader.hidden = true;
-        initialLoad = false;
-        count = 30;
     }
 }
 
 // Helper Function to Set Attributes on DOM Elements
 function setAttributes(element, attributes) {
     for (const key in attributes) {
-        element.addAttribute(key, attributes[key]);
+        element.setAttribute(key, attributes[key]);
     }
 }
 
@@ -36,21 +33,21 @@ function displayPhotos() {
     totalImages = photosArray.length;
     // Run function for each object in photosArray
     photosArray.forEach((photo) => {
-        // Create <a> to link to Unsplash
+        // Create <a> to link to full photo
         const item = document.createElement('a');
         setAttributes(item, {
             href: photo.links.html,
-            target: '_blank'
+            target: '_blank',
         });
         // Create <img> for photo
         const img = document.createElement('img');
         setAttributes(img, {
             src: photo.urls.regular,
             alt: photo.alt_description,
-            title: photo.alt_description
+            title: photo.alt_description,
         });
         // Event Listener, check when each is finished loading
-        img.addEventListener('load', imageLoaded); 
+        img.addEventListener('load', imageLoaded);
         // Put <img> inside <a>, then put both inside imageContainer Element
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -76,5 +73,5 @@ window.addEventListener('scroll', () => {
     }
 });
 
-// On load
+// On Load
 getPhotos();
